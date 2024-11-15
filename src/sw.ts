c6ebc78ceaac6b3799c2fc8b4adb64520f486236
Grey Hill <infogreytech@gmail.com>
2024-11-15 13:00:04 -0800

    Use cache-first strategy

--- src/sw.ts
+++ src/sw.ts
@@ -6,7 +6,7 @@ export type { };
 declare const self: ServiceWorkerGlobalScope;
 
 const CACHE_NAME = "backend";
-const VERSION = "v0.0.1";
+const VERSION = "v0.1.0";
 
 const CURRENT_CACHE_KEY = `${CACHE_NAME}_${VERSION}`;
 
@@ -16,14 +16,13 @@ self.addEventListener("install", event => {
     const fn = async () => {
         const cache = await caches.open(CURRENT_CACHE_KEY);
 
-        cache.addAll(CACHE_RESOURCES)
+        cache.addAll(CACHE_RESOURCES);
     };
 
     event.waitUntil(fn());
 });
 
 self.addEventListener("activate", event => {
-
     const fn = async () => {
         const keys = await caches.keys();
 
@@ -43,34 +42,20 @@ self.addEventListener("fetch", event => {
     const request = event.request;
     const fn: () => Promise<Response> = async () => {
         if (request.mode === "navigate") {
+            // SPA routing; serve root file
             const cachedResponse = await caches.match("/");
             if (cachedResponse) {
                 return cachedResponse;
             }
         }
 
-        const url = new URL(request.url);
-
-        const contentType = request.headers.get("Content-Type");
-        if (contentType === "application/json" && !url.pathname.endsWith(".json")) {
-            const client = await self.clients.get(event.clientId);
-            if (!client) {
-                return;
-            }
-
-            const clients = await self.clients.matchAll();
-
-            client.postMessage(`ServiceWorker: calling api ${url}`);
-
-            // Probably an API call
-            if (url.pathname === "/noise" && request.method === "POST") {
-                return await addNoise(request);
-            } else if (url.pathname === "/noise" && request.method === "GET") {
-                return await getNoise(request);
-            }
+        // PWA files should already be in the cache
+        const cachedResponse = await caches.match(request);
+        if (cachedResponse) {
+            return cachedResponse;
         }
 
-        // Defer to network
+        // Otherwise, use the network
         return await fetch(request);
     }
 
